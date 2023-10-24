0a087acb8816efda89a7edddc6973c34cd22c1d7
RT <rustytrim11@gmail.com>
2023-10-24 15:34:24 -0400

    WsOpCodes and VoiceOpCodes

--- src/config/OpCodes.ts
+++ src/config/OpCodes.ts
@@ -1,70 +1,138 @@
 /**
  * OpCodes will be used to determine what data is being sent and recieved throughout strafe.
  */
-export enum OpCodes {
+export enum WsOpCodes {
   /**
    * Recieved whenever an event was dispatched.
-   * Recieve
+   * Sent by server
    */
   DISPATCH = 0,
 
   /**
    * Fired periodically by a user to keep the connection alive.
-   * Send/Recieve
+   * Sent by client and server
    */
   HEARTBEAT = 1,
 
   /**
    * Starts a new session during the initial handshake (Checks if authentication is initially correct).
-   * Send
+   * Sent by client
    */
   IDENTIFY = 2,
 
   /**
    * Recieved whenever there is an update to a user's presence.
-   * Send
+   * Sent by client
    */
   PRESENCE_UPDATE = 3,
 
   /**
    * 	Sent to join/leave or move between voice channels.
-   * Send
+   * Sent by client
    */
   VOICE_STATE_UPDATE = 4,
 
   /**
    * Resume a previous session that was disconnected.
-   * Send
+   * Sent by client
    */
   RESUME = 5,
 
   /**
    * You should attempt to reconnect and resume immediately when you get this opcode.
-   * Recieve
+   * Sent by server
    */
   RECONNECT = 6,
 
   /**
    * Request information about offline guild members in a large guild.
-   * Send
+   * Sent by client
    */
   REQUEST_GUILD_MEMBERS = 7,
 
   /**
    * The session has been invalidated. You should reconnect and identify/resume accordingly.
-   * Recieve
+   * Sent by server
    */
   INVALID_SESSION = 8,
 
   /**
-   * Sent immediately after connecting, contains the `heartbeat_interval` to use.
-   * Recieve
+   * Sent immediately after connecting, contains the `heartbeatInterval` to use.
+   * Sent by server
    */
   HELLO = 9,
 
   /**
    * Sent in response to receiving a heartbeat to acknowledge that it has been received.
-   * Recieve
+   * Sent by server
    */
   HEARTBEAT_ACK = 10,
+}
+
+export enum VoiceOpCodes {
+  /**
+   * Begin a voice websocket connection.
+   * Sent by client
+   */
+  IDENTIFY = 0,
+
+  /**
+   * Select the voice protocol.
+   * Sent by client
+   */
+  SELECT_PROTOCOL = 1,
+
+  /**
+   * Complete the websocket handshake.
+   * Sent by server
+   */
+  READY = 2,
+
+  /**
+   * Keep the websocket connection alive.
+   * Sent by client
+   */
+  HEARTBEAT = 3,
+
+  /**
+   * Describe the session.
+   * Sent by server
+   */
+  SESSION_DESCRIPTION = 4,
+
+  /**
+   * Indicate which users are speaking.
+   * Sent by client and server
+   */
+  SPEAKING = 5,
+
+  /**
+   * Sent to acknowledge a received client heartbeat.
+   * Sent by server
+   */
+  HEARTBEAT_ACK = 6,
+
+  /**
+   * Resume a connection.
+   * Sent by client
+   */
+  RESUME = 7,
+
+  /**
+   * Time to wait between sending heartbeats in milliseconds.
+   * Sent by server
+   */
+  HELLO = 8,
+
+  /**
+   * Acknowledge a successful session resume.
+   * Sent by server
+   */
+  RESUMED = 9,
+
+  /**
+   * A client has disconnected from the voice channel.
+   * Sent by server
+   */
+  CLIENT_DISCONNECT = 10,
 }
