d44150455890a314dafe8c85bfaaa8e6ab60db28
RT <rustytrim11@gmail.com>
2023-11-06 17:24:30 -0500

    Create and Update Friend Request

--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -3,6 +3,8 @@ import Validator from "../../util/Validator";
 import FriendRequest from "../../database/models/FriendRequest";
 import { Op } from "sequelize";
 import User from "../../database/models/User";
+import { sockets } from "../..";
+import { WsOpCodes } from "../../config/OpCodes";
 const router = express.Router();
 
 router.get("/@me", Validator.verifyToken, (req, res) => {
@@ -62,8 +64,63 @@ router.get("/@me/relationships", Validator.verifyToken, async (req, res) => {
   res.status(200).json({ code: 200, data: requests });
 });
 
-router.post("/@me/relationships", Validator.verifyToken, async (req, res) => {
+router.post("/@me/relationships/:userId", Validator.verifyToken, async (req, res) => {
+  const senderId = req.body.user.id;
+  const receiverId = req.params.userId;
+  if (senderId == receiverId) return res.status(400).json({ code: 400, message: "You cannot add yourself as a friend :/" });
 
-})
+  try {
+    const exists = await FriendRequest.findOne({ where: { senderId, receiverId } });
+    if (exists) return res.status(400).json({ code: 400, message: "Friend request already sent." });
+    if (!await User.findOne({ where: { id: receiverId } })) return res.status(404).json({ code: 404, message: "A user was not found to add as a friend." });
+    const newRequest = await FriendRequest.create({
+      senderId,
+      receiverId,
+      status: "pending"
+    });
+
+    sockets.get(senderId)?.send(JSON.stringify({ op: WsOpCodes.FRIEND_REQUEST, data: newRequest }));
+    sockets.get(receiverId)?.send(JSON.stringify({ op: WsOpCodes.FRIEND_REQUEST, data: newRequest }));
+
+    return res.status(201).json({ code: 201, data: newRequest });
+  } catch (err) {
+    return res.status(500).json({ code: 500, message: 'Error sending friend request.' });
+  }
+});
+
+router.patch("/@me/relationships/:userId", Validator.verifyToken, async (req, res) => {
+  const senderId = req.body.user.id;
+  const receiverId = req.params.userId;
+  const { action } = req.body;
+
+  try {
+    const request = await FriendRequest.findOne({
+      where: {
+        senderId,
+        receiverId,
+        status: "pending",
+      },
+    });
+
+    if (!request) return res.status(404).json({ code: 404, message: "Friend request not found." });
+    switch (action) {
+      case "accept":
+        request.status = "accepted";
+        await request.save();
+        sockets.get(senderId)?.send(JSON.stringify({ op: WsOpCodes.FRIEND_REQUEST, data: request }));
+        sockets.get(receiverId)?.send(JSON.stringify({ op: WsOpCodes.FRIEND_REQUEST, data: request }));
+        break;
+      case "reject":
+        request.status = "rejected";
+        await request.save();
+        sockets.get(senderId)?.send(JSON.stringify({ op: WsOpCodes.FRIEND_REQUEST, data: request }));
+        break;
+      default:
+        return res.status(400).json({ code: 400, message: "Invalid action for friend request." });
+    }
+  } catch (err) {
+    res.status(500).json({ code: 500, message: "Error updating friend request." });
+  }
+});
 
 export default router;
