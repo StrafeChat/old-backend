5b2d63301e3f74bd20669c68df10096915a1529b
RT <rustytrim11@gmail.com>
2023-11-15 01:17:22 -0500

    Co-authored-by: Bryden Calaway <BrydenIsNotSmart@users.noreply.github.com> Co-authored-by: ForGetFulSkyBro <forgetfulskybro@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,15 @@ const heartbeatInterval = 10000;
 
 dotenv.config();
 
+// const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	// store: ... , // Use an external store for consistency across multiple server instances.
+// })
+
+// app.use(limiter);
 app.use(bodyParser.json());
 app.use(
   cors({
--- src/routes/v1/rooms.ts
+++ src/routes/v1/rooms.ts
@@ -7,61 +7,78 @@ import { sockets } from "../..";
 import { WsOpCodes } from "../../config/OpCodes";
 import User from "../../database/models/User";
 const router = express.Router();
+import { rateLimit } from "express-rate-limit";
 
-router.post("/:roomId/messages", Validation.verifyToken, async (req, res) => {
-  console.log(req.body.attachments)
-  if (!req.params.roomId)
-    return res.status(401).json({ code: 401, message: "Room ID is required!" });
-  const room = await Room.findByPk(req.params.roomId, {
-    include: [
-      {
-        model: User,
-        as: "recipients",
-      },
-    ],
-  });
-  if (!room)
-    return res.status(404).json({
-      code: 404,
-      message: "The room you were looking for does not exist.",
+router.post(
+  "/:roomId/messages",
+  Validation.verifyToken,
+  rateLimit({
+    windowMs: 3000,
+    limit: 5,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
+  }),
+  async (req, res) => {
+    console.log(req.body.content);
+    if (!req.params.roomId)
+      return res
+        .status(401)
+        .json({ code: 401, message: "Room ID is required!" });
+    const room = await Room.findByPk(req.params.roomId, {
+      include: [
+        {
+          model: User,
+          as: "recipients",
+        },
+      ],
     });
+    if (!room)
+      return res.status(404).json({
+        code: 404,
+        message: "The room you were looking for does not exist.",
+      });
 
-  switch (room.type) {
-    case 0:
-      if (!room.recipients?.find((user) => user.id == req.body.user.id)) {
-        console.log("No Access!");
-        return res
-          .status(403)
-          .json({ code: 403, message: "You do not have access to this room." });
-      }
-
-      await Message.create({
-        id: Generator.generateSnowflake(),
-        authorId: req.body.user.id,
-        content: req.body.content,
-        embeds: req.body.embeds,
-        roomId: req.params.roomId,
-      }).then(async (message) => {
-        await message.reload({ include: User });
-        for (const user of room.recipients!) {
-          sockets.get(user.id)?.send(
-            JSON.stringify({
-              op: WsOpCodes.DISPATCH,
-              event: "MESSAGE_CREATE",
-              data: message,
-            })
-          );
+    switch (room.type) {
+      case 0:
+        if (!room.recipients?.find((user) => user.id == req.body.user.id)) {
+          console.log("No Access!");
+          return res
+            .status(403)
+            .json({
+              code: 403,
+              message: "You do not have access to this room.",
+            });
         }
-      });
-      break;
-    default:
-      res.status(403).json({
-        code: 403,
-        message: "This channel does not support message sending.",
-      });
-      break;
+
+        await Message.create({
+          id: Generator.generateSnowflake(),
+          authorId: req.body.user.id,
+          content: req.body.content,
+          embeds: req.body.embeds,
+          roomId: req.params.roomId,
+        }).then(async (message) => {
+          await message.reload({ include: User });
+          for (const user of room.recipients!) {
+            sockets.get(user.id)?.send(
+              JSON.stringify({
+                op: WsOpCodes.DISPATCH,
+                event: "MESSAGE_CREATE",
+                data: message,
+              })
+            );
+          }
+        });
+        res.status(200).send("Done!");
+        break;
+      default:
+        res.status(403).json({
+          code: 403,
+          message: "This channel does not support message sending.",
+        });
+        break;
+    }
   }
-});
+);
 
 router.get("/:roomId/messages", Validation.verifyToken, async (req, res) => {
   if (!req.params.roomId)
@@ -128,7 +145,7 @@ router.get("/:roomId/messages", Validation.verifyToken, async (req, res) => {
     default:
       res.status(403).json({
         code: 403,
-        message: "This channel does not support message sending.",
+        message: "This channel does not have any messages.",
       });
       break;
   }
